5276dc50a49010ca88f6ee3469459a0a0c6c2acc
govardhan-2005 <govamanoj6@gmail.com>
2026-08-08 23:58:59 +0530

    added Bank.java

---
+++ Bank.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+class Bank {
+    double balance = 10000;
+
+    void deposit(double amount) {
+        balance = balance + amount;
+        System.out.println("Amount Deposited: " + amount);
+    }
+
+    void withdraw(double amount) {
+        if (amount <= balance) {
+            balance = balance - amount;
+            System.out.println("Amount Withdrawn: " + amount);
+        } else {
+            System.out.println("Insufficient Balance");
+        }
+    }
+
+    void displayBalance() {
+        System.out.println("Balance: " + balance);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Bank b = new Bank();
+
+        b.displayBalance();
+
+        System.out.print("Enter deposit amount: ");
+        double d = sc.nextDouble();
+        b.deposit(d);
+
+        System.out.print("Enter withdrawal amount: ");
+        double w = sc.nextDouble();
+        b.withdraw(w);
+
+        b.displayBalance();
+    }
+}
