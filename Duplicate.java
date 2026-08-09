5b0d77c0a8cc6ee64faef11ce1f0408eae1e82f0
govardhan-2005 <govamanoj6@gmail.com>
2026-08-09 22:53:45 +0530

    added Duplicate.java

---
+++ Duplicate.java
@@ -0,0 +1,13 @@
+class Duplicate {
+    public static void main(String[] args) {
+        int[] a = {4, 2, 7, 3, 2, 8, 5};
+
+        for (int i = 0; i < a.length; i++) {
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[i] == a[j]) {
+                    System.out.println("Duplicate element = " + a[i]);
+                }
+            }
+        }
+    }
+}
