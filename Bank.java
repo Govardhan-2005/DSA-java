8fba0f04e60f152fce67ffaf9a50bd26f2aa0a6c
govardhan-2005 <govamanoj6@gmail.com>
2026-08-09 22:55:04 +0530

    added Bank.java

--- Bank.java
+++ Bank.java
@@ -22,20 +22,20 @@ class Bank {
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        try (Scanner sc = new Scanner(System.in)) {
+            Bank b = new Bank();
 
-        Bank b = new Bank();
+            b.displayBalance();
 
-        b.displayBalance();
+            System.out.print("Enter deposit amount: ");
+            double d = sc.nextDouble();
+            b.deposit(d);
 
-        System.out.print("Enter deposit amount: ");
-        double d = sc.nextDouble();
-        b.deposit(d);
+            System.out.print("Enter withdrawal amount: ");
+            double w = sc.nextDouble();
+            b.withdraw(w);
 
-        System.out.print("Enter withdrawal amount: ");
-        double w = sc.nextDouble();
-        b.withdraw(w);
-
-        b.displayBalance();
+            b.displayBalance();
+        }
     }
 }
