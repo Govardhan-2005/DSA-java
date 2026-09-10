c877f7ba2d69a75c14f0a1575e5944ffc17c489f
govardhan-2005 <govamanoj6@gmail.com>
2026-09-10 20:32:56 +0530

    added Search.java

--- Search.java
+++ Search.java
@@ -1,1 +1,17 @@
 
+static void search(int value) {
+
+    Node temp = head;
+
+    while (temp != null) {
+
+        if (temp.data == value) {
+            System.out.println("Element found");
+            return;
+        }
+
+        temp = temp.next;
+    }
+
+    System.out.println("Element not found");
+}
