22b4cd3c0663c5e7b6b259c7e00fc4ad34465d19
govardhan-2005 <govamanoj6@gmail.com>
2026-09-10 20:29:39 +0530

    added Singlelinksort.java

--- Singlelinksort.java
+++ Singlelinksort.java
@@ -1,3 +1,55 @@
 public class Singlelinksort {
-    
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    static Node head = null;
+
+    // Insert at end
+    static void insert(int data) {
+
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        Node temp = head;
+
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+
+        temp.next = newNode;
+    }
+
+    // Display
+    static void display() {
+
+        Node temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+
+        System.out.println("NULL");
+    }
+
+    public static void main(String[] args) {
+
+        insert(10);
+        insert(20);
+        insert(30);
+        insert(40);
+
+        display();
+    }
 }
